c7223cd73fb2bd5c9a8109365a3d365e3733aad6
marcantoine <marcantoine.guery@gmail.com>
2020-01-23 17:52:36 +0100

    Different alts are now printed on different lines

--- varif.py
+++ varif.py
@@ -59,7 +59,6 @@ class Varif(object):
         startIndex=position-1
         endIndex=position+len(reference)-1
         if strand=="+":
-            print(position)
             phase=(phase+startIndex-2+1-startCDS)%3
         elif strand=="-":
             phase=(phase+endIndex+2-endCDS)%3
@@ -75,8 +74,8 @@ class Varif(object):
         code=0 if fixed is True else 1
         code=-math.inf if allVariants is True else code
         sortedKeys=sorted(self.variants.variants, key= lambda x : (x.split(":")[0], int(x.split(":")[1].split(".")[0])))
-        printedLine="Chromsome, Position, Type, Ref, Alt, AAref, AAalts, Score, Annotation,"
-        printedLine+=",".join(self.variants.samples)+"\n"
+        printedLine="Chromsome; Position; Type; Ref; Alt; AAref; AAalt; Annotation; Score;"
+        printedLine+=";".join(self.variants.samples)+"\n"
         for key in sortedKeys:
             if not any(score>=code for score in self.variants.variants[key]["scores"]):
                 continue
@@ -95,63 +94,25 @@ class Varif(object):
                     aaDiff=self.get_aa_from_mutation(chrom, pos, self.variants.variants[key]["ref"], alt, gffId)
                     self.variants.variants[key]["aaAlts"][gffId].append(aaDiff[1])
                 self.variants.variants[key]["aaRef"][gffId]=aaDiff[0]
-            printedLine+=key.split(":")[0]+","
-            printedLine+=key.split(":")[1]+","
-            printedLine+=self.variants.variants[key]["category"]+","
-            printedLine+=self.variants.variants[key]["ref"]+","
-            printedLine+=";".join(self.variants.variants[key]["alts"])+","
-            if len(gffIds) > 0:
-                printedLine+=":".join([self.variants.variants[key]["aaRef"][gffId]+" ("+gffId+")" for gffId in self.variants.variants[key]["aaRef"]])+","
-                printedLine+=":".join([";".join(self.variants.variants[key]["aaAlts"][gffId])+" ("+gffId+")" for gffId in self.variants.variants[key]["aaAlts"]])+","
-                printedLine+=":".join([self.annotations.annotations[gffId]['description']+" ("+gffId+")" for gffId in gffIds])+","
-            else:
-                printedLine+="NA,"
-                printedLine+="NA,"
-                printedLine+="NA,"
-            printedLine+=";".join([str(score) for score in self.variants.variants[key]["scores"]])+","
-            for index,sample in enumerate(self.variants.variants[key]["ratios"]):
-                printedLine+=";".join([str(self.variants.variants[key]["ratios"][sample][rank]) for rank in range(1,len(self.variants.variants[key]["ratios"][sample]))])
-                printedLine+="," if index < len(self.variants.variants[key]["ratios"])-1 else "\n"
+            for altIndex, alt in enumerate(self.variants.variants[key]["alts"]):
+                printedLine+=key.split(":")[0]+";"
+                printedLine+=key.split(":")[1]+";"
+                printedLine+=self.variants.variants[key]["category"]+";"
+                printedLine+=self.variants.variants[key]["ref"]+";"
+                printedLine+=alt+";"
+                if len(gffIds) > 0:
+                    printedLine+=":".join([self.variants.variants[key]["aaRef"][gffId]+" ("+gffId+")" for gffId in self.variants.variants[key]["aaRef"]])+";"
+                    printedLine+=":".join([self.variants.variants[key]["aaAlts"][gffId][altIndex] for gffId in self.variants.variants[key]["aaAlts"]])+";"
+                    printedLine+=":".join(set(self.annotations.annotations[gffId]['description'] for gffId in gffIds))+";"
+                else:
+                    printedLine+="NA;"
+                    printedLine+="NA;"
+                    printedLine+="NA;"
+                printedLine+=str(self.variants.variants[key]["scores"][altIndex])+";"
+                for sampIndex,sample in enumerate(self.variants.variants[key]["ratios"]):
+                    printedLine+=str(self.variants.variants[key]["ratios"][sample][altIndex])
+                    printedLine+=";" if sampIndex < len(self.variants.variants[key]["ratios"])-1 else "\n"
         if show is True:
             print(printedLine)
         with open(csv, 'w') as f:
             f.write(printedLine)
-
-        
-    def get_id_from_position(self, position, gffreader, chromosome):
-        """
-        Retrieve information about an annotation level in which there is a variant
-
-        position (int) : the position of the variant
-        gffreader (read_gff) : the gff reader object
-        chromosome (str) : the chromosome name in gff reader
-
-        return (list) : ID, function, start/end positions or None if outside of a variant
-        """
-        finished=False
-        maxValue=len(gffreader[chromosome]['positions'])-1
-        minValue=0
-        index=math.floor(maxValue-minValue/2)
-        while not finished:
-            mini=gffreader[chromosome]['positions'][index][1][0]
-            maxi=gffreader[chromosome]['positions'][index][1][1]
-            if position >= mini and position <= maxi:
-                finished=True
-                rank=0
-                while gffreader[chromosome]['ids'][rank][0] != gffreader[chromosome]['positions'][index][0]:
-                    rank+=1
-                identifier=gffreader[chromosome]['ids'][rank][1]
-                description=gffreader[chromosome]['descriptions'][rank][1]
-                return [identifier, description, gffreader[chromosome]['positions'][index]]
-            elif position > maxi and index < maxValue:
-                minValue=index+1
-                index=math.ceil((maxValue+index)/2)
-            elif position < mini and index > minValue:
-                maxValue=index-1
-                index=math.floor((index+minValue)/2)
-            else:
-                finished=True
-
-        return ["No ID", "No function", "No Positions"]
-
-    
